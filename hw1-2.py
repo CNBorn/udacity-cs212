ab2c74ddd581648a0d772fe6bd7d57d5b7f35b23
CNBorn <CNBorn@hotmail.com>
2012-04-23 22:27:38 +0800

    rewrite hw1-2 with brute force method, still have one more test to go (from the discussion)

--- hw1-2.py
+++ hw1-2.py
@@ -24,126 +24,79 @@
 # you have 4 kings and 3 queens, there are three best
 # hands: 4 kings along with any of the three queens).
 
-from copy import copy
+from copy import copy, deepcopy
 import itertools
+from itertools import chain, combinations
 
-def best_wild_hand(hand):
+def best_wild_hand(origin_hand):
     "Try all values for jokers in all 5-card selections."
-    hands = itertools.combinations(hand, 5)
+    hands = itertools.combinations(origin_hand, 5)
     hand_pool = []
     for hand in hands:
+        if has_joker(hand):
+            for hand_with_joker_processed in replace_joker_with_cards(hand):
+                hand_pool.append(hand_with_joker_processed)
+        else:
+            hand_pool.append(hand)
 
-        if flush_with_joker(hand) and straight_with_joker(card_ranks_with_joker(hand)):
-            if has_joker_by_suit(hand, get_flush_suit(hand)):
-                result_hand = list(hand)
-                for joker_card in get_joker_card(hand, suit=get_flush_suit(hand)):
-                    result_hand.remove(joker_card)
-                    result_hand.append(make_card(get_straight_highest_rank(hand)+1,get_flush_suit(hand)))
-                hand_pool.append(result_hand)
-            else:
-                hand_pool.append(hand)
-
-        elif kind_with_joker(4, hand):
-            result_hand = list(hand)
-            for joker_card in get_joker_card(hand):
-                result_hand.remove(joker_card)
-            result_hand.extend(get_missing_4_kind_card(hand)[:len(get_joker_card(hand))])
-            hand_pool.append(result_hand)
-
+    hand_pool = [hands for hands in hand_pool if hands] #[]
     hand_pool.sort(key=lambda hand:hand_rank(hand), reverse=True)
     return hand_pool[0]
 
-def get_missing_4_kind_card(hand):
-    ranks = card_ranks(hand)
-    missing_cards, missing_rank, missing_suit = get_missing_for_kind(4, hand)
-    results = []
-    for (missing_rank, missing_suit) in itertools.product(missing_rank, missing_suit):
-        results.append(make_card(missing_rank, missing_suit))
-    return results
+def replace_joker_with_cards(hand):
+    jokers = get_joker_card(hand)
+    for joker_combination in powerset(jokers):
+        result = get_hand_replaced_with_joker_list(hand, joker_combination)
+        yield list(result)
 
-def get_missing_for_kind(n, hand):
-    remain_suits = ["C", "H", "D", "S"]
-    used_suits = []
-    original_hand = copy(hand)
-    hand = [card for card in hand if card[0] != "?"] #filter out joker
-    max_result = max([(k, len(list(g))) for k,g in itertools.groupby([r for r,s in hand])])
-    max_rank, max_rank_count = max_result
-    missing_cards = n - max_rank_count
-    missing_rank = max_rank
-    for r,s in original_hand:
-        if r == missing_rank and s in remain_suits:
-            used_suits.append(s)
-            remain_suits.remove(s)
+def get_hand_replaced_with_joker_list(hand, joker_combination):
 
-    for r,s in original_hand:
-        if r == "?":
-            joker_suits = get_joker_can_suit(s)
-            remain_suits.extend(list(set(joker_suits) & set(remain_suits) - set(used_suits)))
+    suit_list = []
+    available_suits = []
+    hand_without_joker = list(deepcopy(hand))
 
-    return missing_cards, missing_rank, list(set(remain_suits))
+    for rj, cj in joker_combination:
+        hand_without_joker.remove("%s%s" % (rj, cj)) #remove joker
+        suit_list.extend(get_joker_can_suit(cj))
+
+    available_suits.extend(itertools.product(["23456789TJQKA"], suit_list))
+
+    cards = []
+    available_cards = []
+    for r, s in available_suits:
+        available_cards.append(itertools.product(r, s))
+    available_ranks_suits = [list(i) for i in available_cards]
+   
+    hand_pool = []
+    for t in available_ranks_suits:
+        cards.extend(["%s%s" % (r,s) for (r, s) in t])
+
+    #combinations for n_joker cards
+    for card_set in combinations(cards, len(joker_combination)):
+        r = list(itertools.chain.from_iterable([hand_without_joker, card_set]))
+        r = list(set(r))
+        #print card_set, r
+        if len(r) == 5 and not has_joker(list(r)):
+            hand_pool.append(list(r))
+
+    hand_pool.sort(key=lambda hand:hand_rank(hand), reverse=True)
+    return hand_pool and hand_pool[0] or []
+
+#good stuff comes from Recipe
+def powerset(iterable):
+    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
 def get_joker_can_suit(color):
     color_suit = {"B":["S", "C"], "R":["H", "D"]}
     return color_suit[color]
-
-def kind_with_joker(n, hand):
-    """Return the first rank that this hand has 
-    exactly n-of-a-kind of. Return None if there 
-    is no n-of-a-kind in the hand."""
-    ranks = card_ranks_with_joker(hand)
-    joker_count = ranks.count(15)
-    for r in ranks:
-        if ranks.count(r) == n or ranks.count(r) + joker_count == n:
-            return r
-    return None
-
-def get_hand_lowest_card(hand):
-    min_rank = str(min(card_ranks_with_joker(hand)))
-    for r,s in hand:
-        if make_card(min_rank, s) == "%s%s" % (r,s):
-            return make_card(min_rank, s)
-
-def card_ranks_with_joker(hand):
-    ranks = ['--23456789TJQKA?'.index(r) for r, s in hand]
-    ranks.sort(reverse = True)
-    return [5, 4, 3, 2, 1] if (ranks == [14, 5, 4, 3, 2]) else ranks
-
-def flush_with_joker(hand):
-    suits = [s for r,s in hand]
-    return len(set(suits)) == 1 or (len(set(suits)) == 2 and has_joker(hand))
-
-def straight_with_joker(ranks):
-    """Return True if the ordered 
-    ranks form a 5-card straight."""
-    ranks_without_joker = copy(ranks)
-    if 15 in ranks:
-        ranks_without_joker.remove(15)
-    return ((max(ranks)-min(ranks) == 4) and len(set(ranks)) == 5) or \
-            ((max(ranks_without_joker)-min(ranks_without_joker) == 3) and len(set(ranks_without_joker)) < 5 and 15 in ranks)
-
-def make_card(rank, suit):
-    rank_str = '-A23456789TJQKA'
-    if isinstance(rank, int):
-        rank = rank_str[rank] 
-    return "%s%s" % (rank, suit)
-
-def get_flush_suit(hand):
-    suits = [s for r,s in hand]
-    return list(set(suits))[0]# and len(set(suits)) == 1
-
-def get_straight_highest_rank(hand):
-    ranks = card_ranks(hand)
-    return max(ranks)# and stright(ranks)
 
 def get_joker_card(hand, suit=None):
     if suit:
         suit_color = {"S":"B", "C":"B", "H":"R", "D":"R"}
         return ["%s%s" % (r,s) for r, s in hand if r == "?" and s in suit_color[suit]]
     return ["%s%s" % (r, s) for r, s in hand if r == "?"]
-
-def has_joker_by_suit(hand, suit):
-    suit_color = {"S":"B", "C":"B", "H":"R", "D":"R"}
-    return any([r == "?" and s in suit_color[suit] for r, s in hand])
 
 def has_joker(hand, color=None):
     color_suit = {"B":["S", "C"], "R":["H", "D"]}
@@ -156,6 +109,9 @@ def test_best_wild_hand():
             == ['7C', 'TC', 'TD', 'TH', 'TS'])
     assert (sorted(best_wild_hand("JD TC TH 7C 7D 7S 7H".split()))
             == ['7C', '7D', '7H', '7S', 'JD'])
+    print sorted(best_wild_hand("6C 3C 8C 9C 2S ?B ?R".split()))
+    assert (sorted(best_wild_hand("6C 3C 8C 9C 2S ?B ?R".split()))
+            == ['3C', '6C', '8C', '9C', 'AC'])
     return 'test_best_wild_hand passes'
 
 # ------------------
